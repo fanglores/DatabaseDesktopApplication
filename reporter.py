ead1fb16a745271036fdba73c0dbe7d33811a53e
Konstantin Tsaturyan <kostiktsat@gmail.com>
2023-03-25 12:10:23 +0300

    reporter finalizing

--- reporter.py
+++ reporter.py
@@ -5,18 +5,18 @@ import datetime
 
 
 class ExcelReporter:
-    DEFAULT_SHEETNAME = 'Отчет'
+    default_sheetname = 'Отчет'
 
-    def __init__(self, filename = None, sheetname = None):
+    def __init__(self, filename=None, sheetname=None):
         try:
             if sheetname is not None:
-                self.DEFAULT_SHEETNAME = sheetname
+                self.default_sheetname = sheetname
 
-            datetime_postfix = datetime.datetime.strftime("__%d-%m-%Y_%H-%M-%S")
+            datetime_postfix = datetime.datetime.strftime(datetime.datetime.now(), '__%d-%m-%Y_%H-%M-%S')
             if filename is not None:
-                self.DEFAULT_FILENAME = filename + datetime_postfix
+                self.default_filename = filename + datetime_postfix
             else:
-                self.DEFAULT_FILENAME = 'report' + datetime_postfix
+                self.default_filename = 'report' + datetime_postfix
 
         except Exception as e:
             logging.error(type(e).__name__ + ": " + str(e))
@@ -32,18 +32,20 @@ class ExcelReporter:
 
     def __create_book(self):
         try:
-            self.report_book = excel.Workbook(self.DEFAULT_FILENAME)
-            self.report_book.create_sheet(self.DEFAULT_SHEET_NAME, index=0)
+            logging.info('Creating execl book')
+            self.report_book = excel.Workbook()
+            self.report_book.create_sheet(self.default_sheetname, index=0)
         except Exception as e:
             logging.error(type(e).__name__ + ": " + str(e))
 
-    def __write_data(self):
+    def __write_data(self, data):
+        logging.info('Writing data to excel sheet')
         pass
         # insert each data
 
     def __insert_value(self, cell_name, value):
         try:
-            sheet = self.report_book[self.DEFAULT_SHEET_NAME]
+            sheet = self.report_book[self.default_sheetname]
             cell = sheet[cell_name]
             cell.value = value
         except Exception as e:
@@ -51,13 +53,22 @@ class ExcelReporter:
 
     # TODO: add optinon for definind a save path?
     def __save_book(self):
-        self.report_book.save("{}.xlsx".format(self.DEFAULT_FILENAME))
+        try:
+            logging.info('Saving the excel file')
+            self.report_book.save("{}.xlsx".format(self.default_filename))
+        except Exception as e:
+            logging.error(type(e).__name__ + ": " + str(e))
 
     def __write_header(self):
-        logging.critical('not implemented')
-        raise NotImplemented
-        self.insert_value("A2", value='Имя автора')
-        self.insert_value("C2", value="Статус")
-        self.insert_value("D2", value="Важность")
-        self.insert_value("F1", value="Динамика")
-        self.insert_value("J2", value="Время проверки")
+        try:
+            self.__insert_value("A2", value='Имя автора')
+            self.__insert_value("B2", value='Название программы')
+            self.__insert_value("C2", value="Важна")
+            self.__insert_value("D2", value="Активна")
+            self.__insert_value("E2", value="Отложена")
+            self.__insert_value("F2", value="Нестабильна")
+            self.__insert_value("G2", value="Устранена")
+            self.__insert_value("H2", value="Дата обнаружения")
+            self.__insert_value("I2", value="Дата устранения")
+        except Exception as e:
+            logging.error(type(e).__name__ + ": " + str(e))
